40fcbf8091948f713efc70e860b2e0bfe443b400
Sean Pringle <sean.pringle@gmail.com>
2014-07-07 12:37:57 +1000

    partitioning events bug fixes

--- sql/tendril-setup.sql
+++ sql/tendril-setup.sql
@@ -3,14 +3,61 @@ delimiter ;;
 
 drop event if exists tendril_partition_add;;
 create event tendril_partition_add
-    on schedule every 10 minute starts date(now())
+    on schedule every 1 minute starts date(now()) + interval 0 second
     do begin
 
-        set @days  := to_days(now())+1;
-        set @table := 'none';
+        declare tomorrow int default 0;
+        declare logtable varchar(100) default null;
 
-        select @table := t.table_name from (
-            select distinct part.table_name
+        set tomorrow := to_days(now())+1;
+
+        set logtable := (
+            select t.table_name from (
+                select distinct part.table_name
+                from information_schema.partitions part
+                join purge_schedule shed
+                    on part.table_name = shed.table_name
+                where part.table_schema = database()
+                    and part.table_name like '%log'
+                    and part.partition_method = 'RANGE'
+                    and lower(part.partition_expression) like 'to_days%'
+            ) t
+            left join information_schema.partitions p
+                on p.table_schema = database()
+                and t.table_name = p.table_name
+                and p.table_name like '%log'
+                and p.partition_name = concat('p',tomorrow)
+            where p.partition_name is null
+            limit 1
+        );
+
+        if (logtable is not null) then
+
+            set @sql := concat(
+                'alter table ',logtable,' add partition (partition p',tomorrow,' values less than (',(tomorrow+1),'))'
+            );
+
+            prepare stmt from @sql; execute stmt; deallocate prepare stmt;
+
+            insert into event_log values (now(), @sql);
+
+        end if;
+    end ;;
+
+delimiter ;
+
+delimiter ;;
+
+drop event if exists tendril_partition_drop;;
+create event tendril_partition_drop
+    on schedule every 1 minute starts date(now()) + interval 10 second
+    do begin
+
+        declare partname varchar(100) default null;
+        declare logtable varchar(100) default null;
+
+        set logtable := (
+            select part.table_name
             from information_schema.partitions part
             join purge_schedule shed
                 on part.table_name = shed.table_name
@@ -18,19 +65,17 @@ create event tendril_partition_add
                 and part.table_name like '%log'
                 and part.partition_method = 'RANGE'
                 and lower(part.partition_expression) like 'to_days%'
-        ) t
-        left join information_schema.partitions p
-            on p.table_schema = database()
-            and t.table_name = p.table_name
-            and p.table_name like '%log'
-            and p.partition_name = concat('p',@days)
-        where p.partition_name is null
-        limit 1;
+                and part.partition_name = concat('p',to_days(now() - interval shed.days day))
+            limit 1
+        );
 
-        if (@table <> 'none') then
+        if (logtable is not null) then
+
+            set partname := (select concat('p',to_days(now() - interval days day))
+                from purge_schedule shed where table_name = logtable);
 
             set @sql := concat(
-                'alter table ',@table,' add partition (partition p',@days,' values less than (',(@days+1),'))'
+                'alter table ',logtable,' drop partition ',partname
             );
 
             prepare stmt from @sql; execute stmt; deallocate prepare stmt;
@@ -40,36 +85,9 @@ create event tendril_partition_add
         end if;
     end ;;
 
-drop event if exists tendril_partition_add;;
-create event tendril_partition_add
-    on schedule every 10 minute starts date(now()) + interval 1 minute
-    do begin
+delimiter ;
 
-        set @table := 'none';
-
-        select @table := part.table_name
-        from information_schema.partitions part
-        join purge_schedule shed
-            on part.table_name = shed.table_name
-        where part.table_schema = database()
-            and part.table_name like '%log'
-            and part.partition_method = 'RANGE'
-            and lower(part.partition_expression) like 'to_days%'
-            and part.partition_name = concat('p',to_days(now() - interval shed.days day))
-        limit 1;
-
-        if (@table <> 'none') then
-
-            set @sql := concat(
-                'alter table ',@table,' drop partition p',@days
-            );
-
-            prepare stmt from @sql; execute stmt; deallocate prepare stmt;
-
-            insert into event_log values (now(), @sql);
-
-        end if;
-    end ;;
+delimiter ;;
 
 drop event if exists tendril_purge_client_statistics_log;;
 create event tendril_purge_client_statistics_log
